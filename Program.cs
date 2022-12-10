ed8dbd5758a4e516a3256242ac6336eb5e8a415f
Fidan <fidan@MacBook-Pro-Fidan.local>
2022-12-11 00:23:07 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.Write("A ededini daxil edin:");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("B ededini daxil edin:");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{a}%{b}={a % b}");
+
+        Console.ReadKey();
+
+
+    }
+}
+
